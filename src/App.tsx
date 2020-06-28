311278d01e88b4b621efa29b58f7a900a2c2a9dc
MrAwwad <omarawwad010@gmail.com>
2020-06-28 07:26:32 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,220 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+	Card,
+	CardContent,
+	Box,
+	FormGroup,
+	Container,
+	Typography,
+	FormControlLabel,
+	Checkbox,
+	MenuItem,
+	Button,
+	CircularProgress,
+	FormHelperText,
+} from '@material-ui/core';
+import { Field, Formik, Form, ErrorMessage, useField } from 'formik';
+import { array, boolean, mixed, number, object, string } from 'yup';
+
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+
+export interface InvestmentDetails {
+	fullName: string;
+	initialInvestment?: number;
+	investmentRisk: Array<'High' | 'Medium' | 'Low'>;
+	commentAboutInvestmentRisk: string;
+	dependents?: number;
+	acceptedTermsAndConditions: boolean;
+}
+
+const initialValues: InvestmentDetails = {
+	fullName: '',
+	initialInvestment: 0,
+	investmentRisk: [],
+	commentAboutInvestmentRisk: '',
+	dependents: -1,
+	acceptedTermsAndConditions: false,
+};
+
+const validationSchema = object({
+	fullName: string()
+		.matches(/^[A-Za-z]+$/, 'not a valid name')
+		.required('Your name is mandatory!')
+		.min(2)
+		.max(100),
+	initialInvestment: number().required().min(100),
+	dependents: number().required().min(0).max(5),
+	acceptedTermsAndConditions: boolean().oneOf([true], 'must be checked'),
+	investmentRisk: array(string().oneOf(['high', 'medium', 'low'])).min(
+		1,
+		'at least one should be checked'
+	),
+	commentAboutInvestmentRisk: mixed().when('investmentRisk', {
+		is: (investmentRisk: string[]) =>
+			investmentRisk.find((ir) => ir === 'high'),
+		then: string().required().min(20).max(100),
+		otherwise: string().min(20).max(100),
+	}),
+});
+
+const sleep = (millSecs: number) =>
+	new Promise((res) => setTimeout(res, millSecs));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container maxWidth='sm'>
+			<Card>
+				<CardContent>
+					<Typography align='center' color='primary' variant='h5'>
+						New Bank Account
+					</Typography>
+					<Formik
+						validationSchema={validationSchema}
+						initialValues={initialValues}
+						onSubmit={async (values, helpers) => {
+							await sleep(3000);
+							helpers.resetForm();
+							console.log(values);
+						}}
+					>
+						{({ values, isSubmitting, isValidating, errors, getFieldMeta }) => (
+							<Form>
+								<Box marginBottom={2} marginTop={2}>
+									<Field
+										fullWidth
+										name='fullName'
+										component={TextField}
+										label='Full Name'
+									/>
+								</Box>
+								<Box marginBottom={2} marginTop={1}>
+									<Field
+										fullWidth
+										name='initialInvestment'
+										type='number'
+										component={TextField}
+										label='Initial Investment'
+									/>
+								</Box>
+								<Box>
+									<label>Select the risk you want to take: </label>
+									<Field
+										color='primary'
+										component={CheckboxWithLabel}
+										type='checkbox'
+										name='investmentRisk'
+										value='high'
+										Label={{ label: 'High' }}
+									/>
+									<Field
+										color='primary'
+										component={CheckboxWithLabel}
+										type='checkbox'
+										name='investmentRisk'
+										value='medium'
+										Label={{ label: 'Medium' }}
+									/>
+									<Field
+										color='primary'
+										component={CheckboxWithLabel}
+										type='checkbox'
+										name='investmentRisk'
+										value='low'
+										Label={{ label: 'Low' }}
+									/>
+									{getFieldMeta('investmentRisk').error ? (
+										<FormHelperText error>
+											{getFieldMeta('investmentRisk').error}
+										</FormHelperText>
+									) : null}
+								</Box>
+								<Box marginBottom={2}>
+									<Field
+										fullWidth
+										name='commentAboutInvestmentRisk'
+										component={TextField}
+										multiline
+										rows={3}
+										rowsMax={10}
+										label='Comment About Investment Risk'
+									/>
+								</Box>
+								<Box marginBottom={2}>
+									<Field
+										fullWidth
+										name='dependents'
+										label='dependents'
+										component={TextField}
+										select
+									>
+										<MenuItem value={-1}>Select ...</MenuItem>
+										<MenuItem value={0}>0</MenuItem>
+										<MenuItem value={1}>1</MenuItem>
+										<MenuItem value={2}>2</MenuItem>
+										<MenuItem value={3}>3</MenuItem>
+										<MenuItem value={4}>4</MenuItem>
+										<MenuItem value={5}>5</MenuItem>
+									</Field>
+								</Box>
+
+								<Box marginBottom={2}>
+									<Field
+										color='primary'
+										component={CheckboxWithLabel}
+										type='checkbox'
+										name='acceptedTermsAndConditions'
+										Label={{ label: 'Accept terms and conditions' }}
+									/>
+									{getFieldMeta('acceptedTermsAndConditions').error ? (
+										<FormHelperText error>
+											{getFieldMeta('acceptedTermsAndConditions').error}
+										</FormHelperText>
+									) : null}
+								</Box>
+
+								<Button
+									startIcon={
+										isSubmitting || isValidating ? (
+											<CircularProgress size='1rem' />
+										) : null
+									}
+									variant='contained'
+									color='primary'
+									type='submit'
+									disabled={isSubmitting || isValidating}
+								>
+									Submit
+								</Button>
+							</Form>
+						)}
+					</Formik>
+				</CardContent>
+			</Card>
+		</Container>
+	);
 }
 
 export default App;
+
+// interface LabeledCheckboxProps {
+// 	name: string;
+// 	label: string;
+// 	value?: string | number;
+// }
+
+// const LabeledCheckbox: React.FC<LabeledCheckboxProps> = ({
+// 	label,
+// 	...props
+// }) => {
+// 	const [field] = useField({
+// 		type: 'checkbox',
+// 		...props,
+// 	});
+
+// 	return (
+// 		<FormControlLabel
+// 			control={<Checkbox color='primary' {...props} {...field} />}
+// 			label={label}
+// 		/>
+// 	);
+// };
---
+++ src/try.tsx
@@ -0,0 +1,196 @@
+import {
+	Box,
+	Button,
+	Card,
+	CardContent,
+	Checkbox,
+	CheckboxProps,
+	FormControlLabel,
+	FormGroup,
+	MenuItem,
+	TextField,
+	Typography,
+} from '@material-ui/core';
+import { ErrorMessage, Field, Form, Formik, useField } from 'formik';
+import React from 'react';
+import { array, boolean, mixed, number, object, string } from 'yup';
+
+export interface InvestmentDetails {
+	fullName: string;
+	initialInvestment?: number;
+	investmentRisk: Array<'High' | 'Medium' | 'Low'>;
+	commentAboutInvestmentRisk: string;
+	dependents?: number;
+	acceptedTermsAndConditions: boolean;
+}
+
+const initialValues: InvestmentDetails = {
+	fullName: '',
+	initialInvestment: 0,
+	investmentRisk: [],
+	commentAboutInvestmentRisk: '',
+	dependents: -1,
+	acceptedTermsAndConditions: false,
+};
+
+export function FormDemo() {
+	return (
+		<Card>
+			<CardContent>
+				<Typography variant='h4'>New Account</Typography>
+
+				<Formik
+					validationSchema={object({
+						fullName: string()
+							.required('Your name is mandatory!!!')
+							.min(2)
+							.max(100),
+						initialInvestment: number().required().min(100),
+						dependents: number().required().min(0).max(5),
+						acceptedTermsAndConditions: boolean().oneOf([true]),
+						investmentRisk: array(
+							string().oneOf(['High', 'Medium', 'Low'])
+						).min(1),
+						commentAboutInvestmentRisk: mixed().when('investmentRisk', {
+							is: (investmentRisk: string[]) =>
+								investmentRisk.find((ir) => ir === 'High'),
+							then: string().required().min(20).max(100),
+							otherwise: string().min(20).max(100),
+						}),
+					})}
+					initialValues={initialValues}
+					onSubmit={(values, formikHelpers) => {
+						return new Promise((res) => {
+							setTimeout(() => {
+								console.log(values);
+								console.log(formikHelpers);
+								console.log('---------');
+								res();
+							}, 5000);
+						});
+					}}
+				>
+					{({ values, errors, isSubmitting, isValidating }) => (
+						<Form>
+							<Box marginBottom={2}>
+								<FormGroup>
+									<Field name='fullName' as={TextField} label='Full Name' />
+									<ErrorMessage name='fullName' />
+								</FormGroup>
+							</Box>
+
+							<Box marginBottom={2}>
+								<FormGroup>
+									<Field
+										name='initialInvestment'
+										type='number'
+										as={TextField}
+										label='Initial Investment'
+									/>
+									<ErrorMessage name='initialInvestment' />
+								</FormGroup>
+							</Box>
+
+							<Box marginBottom={2}>
+								<label>Select the risk you want to take:</label>
+								<FormGroup>
+									<MyCheckbox
+										name='investmentRisk'
+										value='High'
+										label='High - Super Risky'
+									/>
+									<MyCheckbox
+										name='investmentRisk'
+										value='Medium'
+										label='Medium - Risky'
+									/>
+									<MyCheckbox
+										name='investmentRisk'
+										value='Low'
+										label='Low - Safe'
+									/>
+									<MyCheckbox
+										name='investmentRisk'
+										value='Very low'
+										label='Very low'
+									/>
+								</FormGroup>
+								<ErrorMessage name='investmentRisk' />
+							</Box>
+							<Box marginBottom={2}>
+								<FormGroup>
+									<Field
+										name='commentAboutInvestmentRisk'
+										as={TextField}
+										multiline
+										rows={3}
+										rowsMax={10}
+										label='Comment About Investment Risk'
+									/>
+									<ErrorMessage name='commentAboutInvestmentRisk' />
+								</FormGroup>
+							</Box>
+
+							<Box marginBottom={2}>
+								<FormGroup>
+									<Field
+										name='dependents'
+										label='dependents'
+										as={TextField}
+										select
+									>
+										<MenuItem value={-1}>Select ...</MenuItem>
+										<MenuItem value={0}>0</MenuItem>
+										<MenuItem value={1}>1</MenuItem>
+										<MenuItem value={2}>2</MenuItem>
+										<MenuItem value={3}>3</MenuItem>
+										<MenuItem value={4}>4</MenuItem>
+										<MenuItem value={5}>5</MenuItem>
+									</Field>
+									<ErrorMessage name='dependents' />
+								</FormGroup>
+							</Box>
+
+							<Box marginBottom={2}>
+								<FormGroup>
+									<MyCheckbox
+										name='acceptedTermsAndConditions'
+										label='Accept terms and conditions'
+									/>
+									<ErrorMessage name='acceptedTermsAndConditions' />
+								</FormGroup>
+							</Box>
+
+							<Button type='submit' disabled={isSubmitting || isValidating}>
+								Submit
+							</Button>
+
+							<pre>{JSON.stringify(errors, null, 4)}</pre>
+							<pre>{JSON.stringify(values, null, 4)}</pre>
+						</Form>
+					)}
+				</Formik>
+			</CardContent>
+		</Card>
+	);
+}
+
+export interface MyCheckboxProps extends CheckboxProps {
+	name: string;
+	value?: string | number;
+	label?: string;
+}
+
+export function MyCheckbox(props: MyCheckboxProps) {
+	const [field] = useField({
+		name: props.name,
+		type: 'checkbox',
+		value: props.value,
+	});
+	return (
+		<FormControlLabel
+			control={<Checkbox {...props} {...field} />}
+			label={props.label}
+		/>
+	);
+}
